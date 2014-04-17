8082c79cbd770b6f4be93e8e4d7bd486dbcf6ffc
Sean Fogle <sfogle@belvederetrading.com>
2014-04-17 14:58:42 -0400

    [8493] Tracker choices from  selected projects in filtering
    The first release of Ethan (the super cool search function for
    Belvedere's Redmine site) had a bug where if you did a search,
    then changed the "Projects" dropdown to be something that had
    more trackers than what you just searched for, it would not add
    these tracker types to the search.
    
    This fixes the tracker checkboxes so that they will display
    according to what projects you have selected, i.e. if you are
    on a project with "Bug" and "Feature" then you switch the
    projects you are searching to "All projects" and there are
    other projects that have a "Task" tracker, you will see "Bug",
    "Feature", "Task".
    
    Change-Id: I248fa1a5bb26fe9968f0932fcc5f1ff3e9f89d79

--- lib/ethan/search_patch.rb
+++ lib/ethan/search_patch.rb
@@ -5,20 +5,20 @@ module Ethan
     module SearchPatch
       def self.included(base) # :nodoc:
         base.send(:include, InstanceMethods)
-    
+
         base.class_eval do
           unloadable
           alias_method_chain :index, :improvements
         end
       end
-    
+
       module InstanceMethods
         def index_with_improvements
           @question = params[:q] || ""
           @question.strip!
           @all_words = params[:all_words] ? params[:all_words].present? : true
           @titles_only = params[:titles_only] ? params[:titles_only].present? : false
-    
+
           projects_to_search =
             case params[:scope]
             when 'all'
@@ -30,16 +30,16 @@ module Ethan
             else
               @project
             end
-    
+
           offset = nil
           begin; offset = params[:offset].to_time if params[:offset]; rescue; end
-    
+
           # quick jump to an issue
           if @question.match(/^#?(\d+)$/) && Issue.visible.find_by_id($1.to_i)
             redirect_to :controller => "issues", :action => "show", :id => $1
             return
           end
-          
+
           @object_types = Redmine::Search.available_search_types.dup
           if projects_to_search.is_a? Project
             # don't search projects
@@ -47,34 +47,32 @@ module Ethan
             # only show what the user is allowed to view
             @object_types = @object_types.select {|o| User.current.allowed_to?("view_#{o}".to_sym, projects_to_search)}
           end
-          
-          if @project.nil?
+
+          if projects_to_search.nil?
             @trackers = Tracker.all
           else
-            @trackers = @project.trackers
+            @trackers = Array(projects_to_search).collect(&:trackers).flatten.uniq
           end
-    
-    
+
           @scope = @object_types.select {|t| params[t]}
           @scope = @object_types if (@scope.empty? )
-          
+
           # extract tokens from the question
           # eg. hello "bye bye" => ["hello", "bye bye"]
           @tokens = @question.scan(%r{((\s|^)"[\s\w]+"(\s|$)|\S+)}).collect {|m| m.first.gsub(%r{(^\s*"\s*|\s*"\s*$)}, '')}
           # tokens must be at least 2 characters long
           @tokens = @tokens.uniq.select {|w| w.length > 1 }
 
-
-          @selected_trackers = @trackers.select {|t| params[t.name] } 
+          @selected_trackers = @trackers.select {|t| params[t.name] }
           @selected_trackers = @trackers if (@selected_trackers.empty? && @scope.any? {|s| s.singularize == "issue" })
-    
+
           if !@tokens.empty?
             # no more than 5 tokens to search for
             @tokens.slice! 5..-1 if @tokens.size > 5
-    
+
             @results = []
             @results_by_type = Hash.new {|h,k| h[k] = 0}
-    
+
             @scope.each do |s|
               model = s.singularize.camelcase.constantize
               if s.singularize == "issue"
@@ -94,7 +92,7 @@ module Ethan
             @question = ""
           end
           render :layout => false if request.xhr?
-    
+
         end
       end
     end
